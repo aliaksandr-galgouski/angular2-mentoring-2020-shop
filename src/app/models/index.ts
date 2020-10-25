5981ec04518fe40a73bd8cf818a782b37b4c9a6c
Aliaksandr Galgouski <Aliaksandr_Galgouski@epam.com>
2020-10-26 01:17:03 +0300

    Removed common services, model, components

--- src/app/models/index.ts
+++
@@ -1,2 +0,0 @@
-export { ProductModel } from './product.model';
-export { ProductCategory } from './product-category.enum';
--- src/app/services/index.ts
+++
@@ -1,2 +0,0 @@
-export { CartService } from './cart.service';
-export { ProductsService } from './products.service';
